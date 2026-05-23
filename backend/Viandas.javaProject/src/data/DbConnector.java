afcf9b76767b6fc457403f956d4e2c786ad4880f
Luisina <lulimoldani@gmail.com>
2026-05-23 18:23:15 -0300

    Primer commit backend

---
+++ backend/Viandas.javaProject/src/data/DbConnector.java
@@ -0,0 +1,50 @@
+package data;
+
+import java.sql.*;
+
+public class DbConnector {
+
+	private static DbConnector instancia;
+	private int conectados = 0;
+	private Connection conn = null;
+	
+	private DbConnector() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static DbConnector getInstancia() {
+		if (instancia == null) {
+			instancia = new DbConnector();
+		}
+		return instancia;
+	}
+	
+	public Connection getConn() {
+		try {
+			if(conn == null || conn.isClosed()) {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/viandas_isabel_cocina","vic_user","tpJava2026");
+				conectados = 0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conectados++;
+		return conn;
+	}
+	
+	public void releaseConn() {
+		conectados--;
+		try {
+			if (conectados <= 0) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
